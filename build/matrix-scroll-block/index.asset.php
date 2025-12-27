e25ae994452dbb9df82b715460adda1cc5e24fe0
Sean Bashaw <seanbshw@gmail.com>
2025-12-26 19:17:43 -0800

    Removed build directory and unused files

--- build/matrix-scroll-block/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'e25e03f201c127e950b6');
--- build/matrix-scroll-block/js/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-interactivity'), 'version' => 'f89ca7078dc733bcc44b');
